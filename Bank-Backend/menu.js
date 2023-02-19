9b01ba2b41a82afa0b32e5ce99245eb2a1ef3b3e
Hemant <hementchaubey@gmail.com>
2023-02-19 19:50:43 +0530

    With the help of async and await made feature where it will keep ask for user for input untill unless user type 6 or more than 6.

--- Bank-Backend/menu.js
+++ Bank-Backend/menu.js
@@ -14,6 +14,35 @@ console.log("\n 4. Check Balance");
 console.log("\n 5. Transfer Money");
 console.log("\n 6. Exit");
 
-rl.question("\n Enter Your Choice : ", (ch) => {
-  console.log(`You entered ${ch}`);
-});
+const ip = () => {
+  return new Promise((resolve, reject) => {
+    rl.question("\n Enter your choice : ", (ch) => {
+      resolve(ch);
+    });
+  });
+};
+
+const startApplication = async () => {
+  while (true) {
+    const choice = await ip();
+
+    if (choice == 1) {
+      console.log(` Create New Account`);
+    } else if (choice == 2) {
+      console.log(` Please Deposit Money`);
+    } else if (choice == 3) {
+      console.log(` Please Withdraw Money`);
+    } else if (choice == 4) {
+      console.log(` Please Check Balance`);
+    } else if (choice == 5) {
+      console.log(` Please Transfer Money`);
+    }
+
+    if (choice >= 6) {
+      console.log(` Exiting the Application..`);
+      process.exit();
+    }
+  }
+};
+
+startApplication();
