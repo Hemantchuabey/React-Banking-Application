16e0f13e1c1cbcc035581d5351fe3c744b519f29
Hemant <hementchaubey@gmail.com>
2023-02-21 17:27:33 +0530

    making structue and connection for database

--- Bank-Backend/app.js
+++ Bank-Backend/app.js
@@ -1,1 +1,13 @@
-console.log("Initialized...");
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+mongoose.set("strictQuery", true);
+app.use(express.json());
+mongoose
+  .connect("mongodb://127.0.0.1:27017/bankDatabase")
+  .then(app.listen(5000))
+  .then(console.log("Connected to Database and Listening to port 5000"))
+  .catch((err) => {
+    console.log(err);
+  });
